c3d23359c19177eb90bff1327126b1aaff975ad5
adamdickey <adickey00@gmail.com>
2023-12-23 22:10:33 -0500

    Rebalanced Havoc, Sever soul, and added Neutralize

---
+++ src/main/java/prismaticmod/cards/Havoc2.java
@@ -0,0 +1,43 @@
+package prismaticmod.cards;
+
+import com.megacrit.cardcrawl.actions.AbstractGameAction;
+import com.megacrit.cardcrawl.actions.common.ApplyPowerAction;
+import com.megacrit.cardcrawl.actions.common.GainBlockAction;
+import com.megacrit.cardcrawl.actions.common.PlayTopCardAction;
+import com.megacrit.cardcrawl.actions.utility.ScryAction;
+import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.core.AbstractCreature;
+import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
+import com.megacrit.cardcrawl.monsters.AbstractMonster;
+import com.megacrit.cardcrawl.powers.AbstractPower;
+import com.megacrit.cardcrawl.powers.watcher.MantraPower;
+import prismaticmod.util.CardStats;
+import theprismatic.ThePrismatic;
+
+public class Havoc2 extends BaseCard {
+    public static final String ID = makeID("Havoc"); //makeID adds the mod ID, so the final ID will be something like "modID:MyCard"
+    private static final CardStats info = new CardStats(
+            CardColor.RED, //The card color. If you're making your own character, it'll look something like this. Otherwise, it'll be CardColor.RED or something similar for a basegame character color.
+            CardType.SKILL, //The type. ATTACK/SKILL/POWER/CURSE/STATUS
+            CardRarity.COMMON, //Rarity. BASIC is for starting cards, then there's COMMON/UNCOMMON/RARE, and then SPECIAL and CURSE. SPECIAL is for cards you only get from events. Curse is for curses, except for special curses like Curse of the Bell and Necronomicurse.
+            CardTarget.SELF, //The target. Single target is ENEMY, all enemies is ALL_ENEMY. Look at cards similar to what you want to see what to use.
+            1 //The card's base cost. -1 is X cost, -2 is no cost for unplayable cards like curses, or Reflex.
+    );
+    //These will be used in the constructor. Technically you can just use the values directly,
+    //but constants at the top of the file are easy to adjust.
+    private static final int scryNumber = 1;
+    public Havoc2() {
+        super(ID, info); //Pass the required information to the BaseCard constructor.
+    }
+    @Override
+    public void use(AbstractPlayer p, AbstractMonster m) {
+        addToBot(new ScryAction(scryNumber));
+        addToBot(new PlayTopCardAction((AbstractCreature)(AbstractDungeon.getCurrRoom()).monsters.getRandomMonster(null, true, AbstractDungeon.cardRandomRng), true));
+    }
+    public void upgrade() {
+        if (!this.upgraded) {
+            upgradeName();
+            upgradeBaseCost(0);
+        }
+    }
+}
