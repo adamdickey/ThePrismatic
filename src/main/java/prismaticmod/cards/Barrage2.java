623251ddfa78b50e1e80aa44ef17f066344b9154
adamdickey <adickey00@gmail.com>
2024-01-07 19:19:31 -0500

    Changed some numbers, added Exploit Weakness

--- src/main/java/prismaticmod/cards/Barrage2.java
+++ src/main/java/prismaticmod/cards/Barrage2.java
@@ -18,7 +18,7 @@ public class Barrage2 extends BaseCard {
     //These will be used in the constructor. Technically you can just use the values directly,
     //but constants at the top of the file are easy to adjust.
 
-    private static final int DAMAGE = 3;
+    private static final int DAMAGE = 4;
     private static final int UPG_DAMAGE = 2;
 
     public Barrage2() {
--- src/main/java/prismaticmod/cards/StormOfSteel2.java
+++ src/main/java/prismaticmod/cards/StormOfSteel2.java
@@ -1,33 +1,30 @@
 package prismaticmod.cards;
 
-import com.megacrit.cardcrawl.actions.common.ApplyPowerAction;
+import com.megacrit.cardcrawl.actions.common.MakeTempCardInHandAction;
+import com.megacrit.cardcrawl.actions.unique.BladeFuryAction;
+import com.megacrit.cardcrawl.cards.tempCards.Shiv;
 import com.megacrit.cardcrawl.characters.AbstractPlayer;
 import com.megacrit.cardcrawl.monsters.AbstractMonster;
-import prismaticmod.powers.OchoPower;
 import prismaticmod.util.CardStats;
-import theprismatic.ThePrismatic;
 
-public class OchoForm extends BaseCard {
-    public static final String ID = makeID("0ch0 Form"); //makeID adds the mod ID, so the final ID will be something like "modID:MyCard"
+public class StormOfSteel2 extends BaseCard {
+    public static final String ID = makeID("Storm of Steel"); //makeID adds the mod ID, so the final ID will be something like "modID:MyCard"
     private static final CardStats info = new CardStats(
-            ThePrismatic.Enums.CARD_COLOR, //The card color. If you're making your own character, it'll look something like this. Otherwise, it'll be CardColor.RED or something similar for a basegame character color.
-            CardType.POWER, //The type. ATTACK/SKILL/POWER/CURSE/STATUS
+            CardColor.GREEN, //The card color. If you're making your own character, it'll look something like this. Otherwise, it'll be CardColor.RED or something similar for a basegame character color.
+            CardType.SKILL, //The type. ATTACK/SKILL/POWER/CURSE/STATUS
             CardRarity.RARE, //Rarity. BASIC is for starting cards, then there's COMMON/UNCOMMON/RARE, and then SPECIAL and CURSE. SPECIAL is for cards you only get from events. Curse is for curses, except for special curses like Curse of the Bell and Necronomicurse.
             CardTarget.SELF, //The target. Single target is ENEMY, all enemies is ALL_ENEMY. Look at cards similar to what you want to see what to use.
-            1 //The card's base cost. -1 is X cost, -2 is no cost for unplayable cards like curses, or Reflex.
+            0 //The card's base cost. -1 is X cost, -2 is no cost for unplayable cards like curses, or Reflex.
     );
     //These will be used in the constructor. Technically you can just use the values directly,
     //but constants at the top of the file are easy to adjust.
-    public OchoForm() {
+
+    public StormOfSteel2() {
         super(ID, info); //Pass the required information to the BaseCard constructor.
+
     }
     @Override
     public void use(AbstractPlayer p, AbstractMonster m) {
-        if(this.upgraded){
-            addToBot(new ApplyPowerAction(p, p, new OchoPower(2), 2));
-        }
-        else{
-            addToBot(new ApplyPowerAction(p, p, new OchoPower(1), 1));
-        }
+        addToBot(new BladeFuryAction(this.upgraded));
     }
 }
